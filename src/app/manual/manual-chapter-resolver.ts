da130edab5600efbce3a9bcabda48a096c5f78b7
Stanislav <k.stanyslaw@gmail.com>
2020-12-09 00:29:35 +0700

    Add lorem

--- src/app/manual/manual-chapter-resolver.ts
+++ src/app/manual/manual-chapter-resolver.ts
@@ -12,7 +12,7 @@ export class ManualChapterResolver implements Resolve<Chapter> {
     const id = route.paramMap.get('id');
     return new Chapter(
       id,
-      'Dummy content'
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sunt modi porro animi unde, aliquid, pariatur repellat quaerat tempore, consequatur molestiae magnam possimus? Adipisci aperiam quaerat, autem, praesentium pariatur enim officiis atque libero magnam tempore et dolor, vitae harum temporibus a quibusdam. Nostrum minima, autem ducimus itaque quod corrupti in? Eligendi tenetur mollitia aut voluptates, exercitationem suscipit, sapiente ut quaerat odit perspiciatis repellendus molestiae doloribus nemo rerum vel, magnam aperiam quod! Quo rerum ipsum id assumenda quaerat. Quaerat voluptates architecto unde fugit vero culpa eligendi ut assumenda nulla, iure necessitatibus nostrum nobis ipsum facere! Voluptates iusto atque iure? Quam, fugit?'
     );
   }
 }
